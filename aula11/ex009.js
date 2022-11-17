144f1f20b3684042258dda4cada5f155b6c4c5b5
elisonlopes <elisonlopez@gmail.com>
2022-11-16 23:36:59 -0300

    Treinando Js

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Gana'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro!')
+} else{
+    console.log('Você é Estrangeiro!')
+}
+//condição composta
